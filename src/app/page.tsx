17b3a60d593241679fd7e63483e59386699eb798
shebll <shebblloll@gmail.com>
2024-05-12 16:29:13 +0300

    form setup to dynamic Field array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,230 @@
-import Image from "next/image";
+"use client";
+import { useForm, useFieldArray } from "react-hook-form";
 
+type formDataType = {
+  name: string;
+  email: string;
+  social: {
+    face: string;
+    insta: string;
+  };
+  phones: string[];
+  address: { value: string }[];
+};
 export default function Home() {
+  const form = useForm<formDataType>({
+    mode: "onChange",
+    defaultValues: {
+      address: [{ value: "" }],
+    },
+  });
+  // distract from form
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = form;
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "address",
+  });
+  // distract from register
+  // const { onBlur, onChange, ref, name } = register("name");
+
+  const onSubmit = (data: formDataType) => {
+    console.log(data);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center gap-10 p-14">
+      <h1 className="text-4xl font-bold">React Hook Form Full Course</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+        className="flex flex-col gap-3"
+      >
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name" className="text-lg font-medium">
+            Name :
+          </label>
+          <input
+            className="bg-gray-200 px-4 py-2 outline-none rounded-lg text-base"
+            type="text"
+            id="name"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "this required",
+              },
+            })}
+          />
+          {errors.name && (
+            <span className="text-red-500">{errors.name.message}</span>
+          )}
         </div>
-      </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email" className="text-lg font-medium">
+            Email :
+          </label>
+          <input
+            className="bg-gray-200 px-4 py-2 outline-none rounded-lg text-base"
+            type="text"
+            id="email"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "this required",
+              },
+              validate: {
+                notAdmin: (felidValue) =>
+                  felidValue !== "admin" || "cant admin",
+              },
+            })}
+          />
+          {errors.email && (
+            <span className="text-red-500">{errors.email.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="social" className="text-lg font-medium">
+            Social :
+          </label>
+          <input
+            className="bg-gray-200 px-4 py-2 outline-none rounded-lg text-base"
+            type="text"
+            id="social"
+            {...register("social.face", {
+              required: {
+                value: true,
+                message: "this required facebook",
+              },
+              validate: {
+                notAdmin: (felidValue) =>
+                  felidValue.startsWith("face") || "not face url",
+              },
+            })}
+          />
+          {errors.social?.face && (
+            <span className="text-red-500">{errors.social?.face.message}</span>
+          )}
+          <input
+            className="bg-gray-200 px-4 py-2 outline-none rounded-lg text-base"
+            type="text"
+            id="social"
+            {...register("social.insta", {
+              required: {
+                value: true,
+                message: "this required insta",
+              },
+              validate: {
+                notAdmin: (felidValue) =>
+                  felidValue.startsWith("insta") || "not insta url",
+              },
+            })}
+          />
+          {errors.social?.insta && (
+            <span className="text-red-500">{errors.social.insta.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="phone-1" className="text-lg font-medium">
+            phone 1:
+          </label>
+          <input
+            className="bg-gray-200 px-4 py-2 outline-none rounded-lg text-base"
+            type="text"
+            id="phone-1"
+            {...register("phones.0", {
+              required: {
+                value: true,
+                message: "this required phone 1",
+              },
+              validate: {
+                notAdmin: (felidValue) =>
+                  felidValue.startsWith("01") || "not good phone",
+              },
+            })}
+          />
+          {errors.phones?.[0] && (
+            <span className="text-red-500">{errors.phones?.[0]?.message}</span>
+          )}
+          <label htmlFor="phone-2" className="text-lg font-medium">
+            phone 2:
+          </label>
+          <input
+            className="bg-gray-200 px-4 py-2 outline-none rounded-lg text-base"
+            type="text"
+            id="phone-2"
+            {...register("phones.1", {
+              required: {
+                value: true,
+                message: "this required phones 2",
+              },
+              validate: {
+                notAdmin: (felidValue) =>
+                  felidValue.startsWith("01") || "not good phone",
+              },
+            })}
+          />
+          {errors.phones?.[1] && (
+            <span className="text-red-500">{errors.phones?.[1]?.message}</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1>Enter Address</h1>
+          {fields.map((field, index) => {
+            return (
+              <div key={index} className="flex flex-col gap-2">
+                <label htmlFor={field.id} className="text-lg font-medium">
+                  address {index + 1}
+                </label>
+                <input
+                  className="bg-gray-200 px-4 py-2 outline-none rounded-lg text-base"
+                  type="text"
+                  id={field.id}
+                  {...register(`address.${index}.value`, {
+                    required: {
+                      value: true,
+                      message: `this required address ${index + 1}`,
+                    },
+                  })}
+                />
+                {index > 0 && (
+                  <button
+                    onClick={() => remove(index)}
+                    className="bg-red-500 text-white text-xl font-bold w-full text-center py-1 rounded-xl"
+                  >
+                    remove this address
+                  </button>
+                )}
+                {errors.address?.[index]?.value && (
+                  <span className="text-red-500">
+                    {errors.address?.[index]?.value.message}
+                  </span>
+                )}
+              </div>
+            );
+          })}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <button
+            type="button"
+            onClick={() => append({ value: "new address" })}
+            className="bg-green-400 text-white text-xl font-bold w-full text-center py-1 rounded-xl"
+          >
+            add new address
+          </button>
+        </div>
+        <div className="">
+          <button
+            type="submit"
+            className="bg-green-500 text-white text-xl font-bold w-full text-center py-1 rounded-xl"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
